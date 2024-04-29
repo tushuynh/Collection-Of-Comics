0e6a278df0c3d3860cb3bad36e61717b64d066b3
tushuynh <huynhtu.1520@gmail.com>
2024-04-29 16:20:43 +0700

    feature(test): crawl new chapter on production

--- src/controllers/ComicController.js
+++ src/controllers/ComicController.js
@@ -156,15 +156,10 @@ class ComicController {
       const response = await axios.get(url, {
         withCredentials: false,
         headers: {
-          host: 'doctruyenonline.vn',
           'Access-Control-Allow-Origin': '*',
           'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-          Accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-          'Accept-Language': 'en-US,en;q=0.9',
-          'Accept-Encoding': 'gzip, deflate, br',
-          Connection: 'keep-alive',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
       });
       const $ = cheerio.load(response.data);
