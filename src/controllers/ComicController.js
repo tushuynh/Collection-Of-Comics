37945f23e517c6c439eb3bea66defc9c88c2dc4e
tushuynh <huynhtu.1520@gmail.com>
2024-04-29 15:07:40 +0700

    feature(test): crawl new chapter on production

--- src/controllers/ComicController.js
+++ src/controllers/ComicController.js
@@ -157,6 +157,9 @@ class ComicController {
         withCredentials: false,
         headers: {
           'Access-Control-Allow-Origin': '*',
+          ':authority': 'doctruyenonline.vn',
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
         },
       });
       const $ = cheerio.load(response.data);
