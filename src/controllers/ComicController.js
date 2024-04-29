abf1da33382a1a2be7a88d1a0a002791a8638904
tushuynh <huynhtu.1520@gmail.com>
2024-04-29 16:25:00 +0700

    feature(test): craw new chapter

--- src/controllers/ComicController.js
+++ src/controllers/ComicController.js
@@ -156,6 +156,7 @@ class ComicController {
       const response = await axios.get(url, {
         withCredentials: false,
         headers: {
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
           'Access-Control-Allow-Origin': '*',
           'User-Agent':
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
