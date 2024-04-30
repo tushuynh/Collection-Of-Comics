3c57ad81bf5c6621f11ec25e5435255c3a207cc7
tushuynk <huynhtu.1520@gmail.com>
2024-04-30 13:12:50 +0700

    feature(test): test crawl

--- src/controllers/ComicController.js
+++ src/controllers/ComicController.js
@@ -6,238 +6,251 @@ const { comicsMongooseToObject } = require('../utils/mongoose');
 const { removeAccents } = require('../utils/helper');
 
 class ComicController {
-  // ------------------------------------------------------------------------- [GET]
-  // [GET] /comics
-  // Home page show all comics
-  showHomepage(req, res, next) {
-    comicSchema
-      .find({
-        userId: req.cookies.userId,
-      })
-      .sort({ lastRead: 1 })
-      .then((comics) => {
-        res.render('pages/comic', {
-          name: req.cookies.name,
-          comics: comicsMongooseToObject(comics),
-        });
-      })
-      .catch((err) => console.log(err));
-  }
+	// ------------------------------------------------------------------------- [GET]
+	// [GET] /comics
+	// Home page show all comics
+	showHomepage(req, res, next) {
+		comicSchema
+			.find({
+				userId: req.cookies.userId,
+			})
+			.sort({ lastRead: 1 })
+			.then((comics) => {
+				res.render('pages/comic', {
+					name: req.cookies.name,
+					comics: comicsMongooseToObject(comics),
+				});
+			})
+			.catch((err) => console.log(err));
+	}
 
-  // [GET] /getComics
-  // Get all comics
-  getComics(req, res, next) {
-    comicSchema
-      .find()
-      .sort({ lastRead: 1 })
-      .then((comics) => res.json(comics))
-      .catch((err) => res.json(err));
-  }
+	// [GET] /getComics
+	// Get all comics
+	getComics(req, res, next) {
+		comicSchema
+			.find()
+			.sort({ lastRead: 1 })
+			.then((comics) => res.json(comics))
+			.catch((err) => res.json(err));
+	}
 
-  // [GET] /updateNewChapter
-  // Craw to update new chapter of all comics
-  async updateNewChapter(req, res, next) {
-    const errorCrawlComics = [];
+	// [GET] /updateNewChapter
+	// Craw to update new chapter of all comics
+	async updateNewChapter(req, res, next) {
+		const errorCrawlComics = [];
 
-    const comics = await comicSchema.find();
+		const comics = await comicSchema.find();
 
-    // Parallel
-    // await Promise.all(
-    //   comics.map(async (comic) => {
-    //     const comicName = removeAccents(comic.name.toLowerCase())
-    //       .split(' ')
-    //       .join('-');
-    //     const url = process.env.WEB_CRAWL_URL + comicName;
+		// Parallel
+		// await Promise.all(
+		//   comics.map(async (comic) => {
+		//     const comicName = removeAccents(comic.name.toLowerCase())
+		//       .split(' ')
+		//       .join('-');
+		//     const url = process.env.WEB_CRAWL_URL + comicName;
 
-    //     try {
-    //       const response = await axios.get(url);
-    //       const $ = cheerio.load(response.data);
+		//     try {
+		//       const response = await axios.get(url);
+		//       const $ = cheerio.load(response.data);
 
-    //       let chap = $('#list-chapter-comic a span').first().text();
-    //       chap = chap.split(' ')[1]?.trim();
-    //       // const imageURL = $('.col-image img').attr('src');
+		//       let chap = $('#list-chapter-comic a span').first().text();
+		//       chap = chap.split(' ')[1]?.trim();
+		//       // const imageURL = $('.col-image img').attr('src');
 
-    //       if (!chap) {
-    //         errorCrawlComics.push({
-    //           comicName,
-    //           url,
-    //           message: 'Can not get new chapter',
-    //         });
-    //       } else if (Number(chap) && chap !== comic.chapPresent) {
-    //         await comicSchema.updateOne(
-    //           {
-    //             name: comic.name,
-    //           },
-    //           {
-    //             chapPresent: chap,
-    //             // image: imageURL,
-    //           }
-    //         );
-    //       }
+		//       if (!chap) {
+		//         errorCrawlComics.push({
+		//           comicName,
+		//           url,
+		//           message: 'Can not get new chapter',
+		//         });
+		//       } else if (Number(chap) && chap !== comic.chapPresent) {
+		//         await comicSchema.updateOne(
+		//           {
+		//             name: comic.name,
+		//           },
+		//           {
+		//             chapPresent: chap,
+		//             // image: imageURL,
+		//           }
+		//         );
+		//       }
 
-    //       return { comicName, chap };
-    //     } catch (error) {
-    //       return errorCrawlComics.push({
-    //         comicName,
-    //         url,
-    //         message: error?.message || '',
-    //       });
-    //     }
-    //   })
-    // );
+		//       return { comicName, chap };
+		//     } catch (error) {
+		//       return errorCrawlComics.push({
+		//         comicName,
+		//         url,
+		//         message: error?.message || '',
+		//       });
+		//     }
+		//   })
+		// );
 
-    // Sequence (handle for rate limit)
-    for (const comic of comics) {
-      const comicName = removeAccents(comic.name.toLowerCase())
-        .split(' ')
-        .join('-');
-      const url = process.env.WEB_CRAWL_URL + comicName;
+		// Sequence (handle for rate limit)
+		for (const comic of comics) {
+			const comicName = removeAccents(comic.name.toLowerCase())
+				.split(' ')
+				.join('-');
+			const url = process.env.WEB_CRAWL_URL + comicName;
 
-      try {
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
+			try {
+				const response = await axios.get(url);
+				const $ = cheerio.load(response.data);
 
-        let chap = $('#list-chapter-comic a span').first().text();
-        chap = chap.split(' ')[1]?.trim();
-        // const imageURL = $('.col-image img').attr('src');
+				let chap = $('#list-chapter-comic a span').first().text();
+				chap = chap.split(' ')[1]?.trim();
+				// const imageURL = $('.col-image img').attr('src');
 
-        if (!chap) {
-          errorCrawlComics.push({
-            comicName,
-            url,
-            message: 'Can not get new chapter',
-          });
-        } else if (Number(chap) && chap !== comic.chapPresent) {
-          await comicSchema.updateOne(
-            {
-              name: comic.name,
-            },
-            {
-              chapPresent: chap,
-              // image: imageURL,
-            }
-          );
-        }
-      } catch (error) {
-        errorCrawlComics.push({
-          comicName,
-          url,
-          message: error?.message || 'Something went wrong!',
-        });
-      }
-    }
+				if (!chap) {
+					errorCrawlComics.push({
+						comicName,
+						url,
+						message: 'Can not get new chapter',
+					});
+				} else if (Number(chap) && chap !== comic.chapPresent) {
+					await comicSchema.updateOne(
+						{
+							name: comic.name,
+						},
+						{
+							chapPresent: chap,
+							// image: imageURL,
+						}
+					);
+				}
+			} catch (error) {
+				errorCrawlComics.push({
+					comicName,
+					url,
+					message: error?.message || 'Something went wrong!',
+				});
+			}
+		}
 
-    return res.json({
-      message: 'Craw new chapter of all comic finished',
-      errorCrawComics: errorCrawlComics.map((error) => {
-        const errorMessage = `[Comic crawl failed] - url: ${error.url} - message: ${error?.message}`;
-        console.error(errorMessage);
-        return errorMessage;
-      }),
-    });
-  }
+		return res.json({
+			message: 'Craw new chapter of all comic finished',
+			errorCrawComics: errorCrawlComics.map((error) => {
+				const errorMessage = `[Comic crawl failed] - url: ${error.url} - message: ${error?.message}`;
+				console.error(errorMessage);
+				return errorMessage;
+			}),
+		});
+	}
 
-  // [GET] /testCrawl
-  // Test crawl new chapter on production
-  async testCrawl(req, res, next) {
-    const errorCrawlComics = [];
-    const comic = {
-      name: 'Bắc Kiếm Giang Hồ',
-      chapPresent: '187',
-    };
+	// [GET] /testCrawl
+	// Test crawl new chapter on production
+	async testCrawl(req, res, next) {
+		const errorCrawlComics = [];
+		const comic = {
+			name: 'Bắc Kiếm Giang Hồ',
+			chapPresent: '187',
+		};
 
-    const comicName = removeAccents(comic.name.toLowerCase())
-      .split(' ')
-      .join('-');
-    const url = process.env.WEB_CRAWL_URL + comicName;
+		const comicName = removeAccents(comic.name.toLowerCase())
+			.split(' ')
+			.join('-');
+		const url = process.env.WEB_CRAWL_URL + comicName;
 
-    try {
-      // const response = await axios.get(url)
-      const response = await axios.get(url, {
-        withCredentials: false,
-        headers: {
-          'Access-Control-Allow-Origin': 'http://localhost:3000',
-          'Access-Control-Allow-Origin': '*',
-          'User-Agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
-      const $ = cheerio.load(response.data);
+		try {
+			// const response = await axios.get(url)
+			const response = await axios.get(url, {
+				withCredentials: false,
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					Referer: 'https://www.google.com/',
+					Accept:
+						'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+					'Accept-Encoding': 'gzip, deflate, br, zstd',
+					'Accept-Language': 'vi,vi-VN;q=0.9,en-US;q=0.8,en;q=0.7',
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Sec-Ch-Ua':
+						'"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+					'Sec-Ch-Ua-Mobile': '?0',
+					'Sec-Ch-Ua-Platform': '"Windows"',
+					'Sec-Fetch-Dest': 'document',
+					'Sec-Fetch-Mode': 'navigate',
+					'Sec-Fetch-Site': 'cross-site',
+					'Sec-Fetch-User': '?1',
+					'Upgrade-Insecure-Requests': '1',
+					'User-Agent':
+						'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+				},
+			});
+			const $ = cheerio.load(response.data);
 
-      let chap = $('#list-chapter-comic a span').first().text();
-      chap = chap.split(' ')[1]?.trim();
-      // const imageURL = $('.col-image img').attr('src');
+			let chap = $('#list-chapter-comic a span').first().text();
+			chap = chap.split(' ')[1]?.trim();
+			// const imageURL = $('.col-image img').attr('src');
 
-      if (!chap) {
-        errorCrawlComics.push({
-          comicName,
-          url,
-          message: 'Can not get new chapter',
-        });
-      } else if (Number(chap) && chap !== comic.chapPresent) {
-        await comicSchema.updateOne(
-          {
-            name: comic.name,
-          },
-          {
-            chapPresent: chap,
-            // image: imageURL,
-          }
-        );
-      }
-    } catch (error) {
-      console.error(`Can not crawl new chapter`);
-      console.error(error);
-      console.error(`Can not crawl new chapter`);
-      errorCrawlComics.push({
-        comicName,
-        url,
-        message: error?.message || 'Something went wrong!',
-      });
-    }
+			if (!chap) {
+				errorCrawlComics.push({
+					comicName,
+					url,
+					message: 'Can not get new chapter',
+				});
+			} else if (Number(chap) && chap !== comic.chapPresent) {
+				await comicSchema.updateOne(
+					{
+						name: comic.name,
+					},
+					{
+						chapPresent: chap,
+						// image: imageURL,
+					}
+				);
+			}
+		} catch (error) {
+			console.error(`Can not crawl new chapter`);
+			console.error(error);
+			console.error(`Can not crawl new chapter`);
+			errorCrawlComics.push({
+				comicName,
+				url,
+				message: error?.message || 'Something went wrong!',
+			});
+		}
 
-    return res.json({
-      message: 'Craw new chapter of all comic finished',
-      errorCrawComics: errorCrawlComics.map((error) => {
-        const errorMessage = `[Comic crawl failed] - url: ${error.url} - message: ${error?.message}`;
-        // console.error(errorMessage);
-        return errorMessage;
-      }),
-    });
-  }
+		return res.json({
+			message: 'Craw new chapter of all comic finished',
+			errorCrawComics: errorCrawlComics.map((error) => {
+				const errorMessage = `[Comic crawl failed] - url: ${error.url} - message: ${error?.message}`;
+				// console.error(errorMessage);
+				return errorMessage;
+			}),
+		});
+	}
 
-  // ---------------------------------------------------------------- [POST]
-  // [POST] /comics
-  createComic(req, res, next) {
-    req.body.userId = req.cookies.userId;
-    const comic = comicSchema(req.body);
-    comic
-      .save()
-      .then(() => res.redirect('back'))
-      .catch((err) => console.log('Error: ', err));
-  }
+	// ---------------------------------------------------------------- [POST]
+	// [POST] /comics
+	createComic(req, res, next) {
+		req.body.userId = req.cookies.userId;
+		const comic = comicSchema(req.body);
+		comic
+			.save()
+			.then(() => res.redirect('back'))
+			.catch((err) => console.log('Error: ', err));
+	}
 
-  // ---------------------------------------------------------------- [PUT]
-  // [PUT] /comics/:_id
-  updateComic(req, res, next) {
-    const { _id } = req.params;
-    req.body.lastRead = new Date();
-    comicSchema
-      .updateOne({ _id }, req.body)
-      .then(() => res.redirect('back'))
-      .catch((err) => console.log('Error: ', err));
-  }
+	// ---------------------------------------------------------------- [PUT]
+	// [PUT] /comics/:_id
+	updateComic(req, res, next) {
+		const { _id } = req.params;
+		req.body.lastRead = new Date();
+		comicSchema
+			.updateOne({ _id }, req.body)
+			.then(() => res.redirect('back'))
+			.catch((err) => console.log('Error: ', err));
+	}
 
-  // ---------------------------------------------------------------- [DELETE]
-  // [DELETE] /comics/:_id
-  deleteComic(req, res, next) {
-    const { _id } = req.params;
-    comicSchema
-      .delete({ _id })
-      .then(() => res.redirect('back'))
-      .catch((err) => console.log('Error: ', err));
-  }
+	// ---------------------------------------------------------------- [DELETE]
+	// [DELETE] /comics/:_id
+	deleteComic(req, res, next) {
+		const { _id } = req.params;
+		comicSchema
+			.delete({ _id })
+			.then(() => res.redirect('back'))
+			.catch((err) => console.log('Error: ', err));
+	}
 }
 
 module.exports = new ComicController();
